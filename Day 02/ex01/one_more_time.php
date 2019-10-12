e072b11fd0297ec920d5fd987ef88725e62139a5
Nqabutho LUNGA <nlunga@c5r7s7.wethinkcode.co.za>
2019-10-12 10:48:05 +0200

    finish

--- Day 02/ex01/one_more_time.php
+++ Day 02/ex01/one_more_time.php
@@ -1,4 +1,8 @@
 #!/usr/bin/php
 <?php
-
+  // Saturday(sometimes upper case in the beginning) 12 October(cap 1st) 2019 08:20:50
+  $timezone_identifiers = DateTimeZone::listIdentifiers();
+  foreach ($timezone_identifiers as $key => $list) {
+    echo $list."\n";
+  }
 ?>
