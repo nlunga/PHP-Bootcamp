9a87d83babdde522550b911de0d327ac920eed56
Nqabutho LUNGA <nlunga@c5r7s7.wethinkcode.co.za>
2019-10-14 09:26:22 +0200

    pst

--- Day 04/ex00/index.php
+++ Day 04/ex00/index.php
@@ -1,3 +1,18 @@
 <?php
-  echo "hello";
+  echo $_GET['name'];
 ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form class="" action="index.html" method="GET">
+      <input type="hidden" name="name" value="Innocent">
+      <button type="submit">PRESS ME!</button>
+    </form>
+  </body>
+</html>
+https://www.youtube.com/watch?v=pkxqlfLioCk
