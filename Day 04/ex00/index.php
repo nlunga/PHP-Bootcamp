c22ebc21d36b5a95083db7a9e0ef65b8dade3743
Innocent <nlunga@student.wethinkcode.co.za>
2019-10-14 12:30:39 +0200

    fix

--- Day 04/ex00/index.php
+++ Day 04/ex00/index.php
@@ -10,7 +10,13 @@
   </head>
   <body>
     <form class="" action="index.html" method="GET">
-      <input type="hidden" name="name" value="Innocent">
+      <label>Enter your username: </label>
+      <input type="text" name="login" value="">
+      <br>
+      <label>Enter your password: </label>
+      <input type="password" name="passwd" value="">
+      <br>
+      <input type="submit" name="submit" value="OK">
       <button type="submit">PRESS ME!</button>
     </form>
   </body>
